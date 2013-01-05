861aefffa31a25b73283128442f0048e47800055
Kristian Sloth Lauszus <lauszus@gmail.com>
2013-01-05 18:18:54 +0100

    Changed url again

--- src/com/tkjelectronics/balanduino/BalanduinoActivity.java
+++ src/com/tkjelectronics/balanduino/BalanduinoActivity.java
@@ -464,7 +464,7 @@ public class BalanduinoActivity extends SherlockFragmentActivity implements
 					.create().show();
 			return true;
 		case android.R.id.home:
-			Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://tkjelectronics.github.com/BalanduinoAndroidApp/"));
+			Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://balanduino.tkjelectronics.com/"));
 			startActivity(browserIntent);
 			return true;
 		default:
