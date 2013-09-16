ce14dd02327acc125d70e901945566c8900ed666
Kristian Sloth Lauszus <lauszus@gmail.com>
2013-09-16 04:01:31 +0200

    Check if it got permission before trying to upload

--- src_usb/Upload.java
+++ src_usb/Upload.java
@@ -1,29 +1,85 @@
 package com.tkjelectronics.balanduino;
 
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbManager;
 import android.util.Log;
 import android.widget.Toast;
 
 import java.io.IOException;
+import java.lang.System;
+import java.util.Map;
 
 import com.physicaloid.lib.Boards;
 import com.physicaloid.lib.Physicaloid;
 import com.physicaloid.lib.Physicaloid.UploadCallBack;
 import com.physicaloid.lib.programmer.avr.UploadErrors;
+import com.tkjelectronics.balanduino.BalanduinoActivity;
 
 public class Upload {
     private static final String TAG = "Upload";
     private static final boolean D = BalanduinoActivity.D;
+    private static final String ACTION_USB_PERMISSION = "com.android.example.USB_PERMISSION";
+    private final static String usbError = "Error opening USB host communication";
+    public final static String flavor = "Usb";
 
     static Physicaloid mPhysicaloid;
-    public final static String flavor = "Usb";
-    final static String usbError = "Error opening USB host communication";
     static boolean uploading = false;
-
 
     public static void UploadFirmware() {
         if (uploading)
             return;
 
+        UsbManager mUsbManager = (UsbManager) BalanduinoActivity.activity.getSystemService(BalanduinoActivity.context.USB_SERVICE);
+        Map<String, UsbDevice> map = mUsbManager.getDeviceList();
+        if (D)
+            Log.i(TAG, "UsbDevices: " + map);
+
+        boolean deviceFound = false;
+        for (Map.Entry<String, UsbDevice> entry : map.entrySet()) {
+            UsbDevice mUsbDevice = entry.getValue();
+            if (mUsbDevice.getVendorId() == 0x0403 && mUsbDevice.getProductId() == 0x6015) { // Check if the robot is connected
+                deviceFound = true;
+                if (mUsbManager.hasPermission(mUsbDevice)) {
+                    if (D)
+                        Log.i(TAG, "Already has permission");
+                    upload();
+                } else {
+                    if (D)
+                        Log.i(TAG, "Requesting permission");
+                    PendingIntent mPermissionIntent = PendingIntent.getBroadcast(BalanduinoActivity.context, 0, new Intent(ACTION_USB_PERMISSION), 0);
+                    BalanduinoActivity.activity.registerReceiver(mUsbReceiver, new IntentFilter(ACTION_USB_PERMISSION));
+                    mUsbManager.requestPermission(mUsbDevice, mPermissionIntent);
+                }
+            }
+        }
+        if (!deviceFound)
+            BalanduinoActivity.showToast("Please connect the Balanduino to the USB Host port", Toast.LENGTH_SHORT);
+    }
+
+    private static final BroadcastReceiver mUsbReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            if (ACTION_USB_PERMISSION.equals(intent.getAction())) {
+                synchronized (this) {
+                    if (intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
+                        if (D)
+                            Log.i(TAG, "Permission allowed");
+                        upload();
+                    }
+                    else {
+                        if (D)
+                            Log.e(TAG, "Permission denied");
+                    }
+                }
+            }
+        }
+    };
+
+    private static void upload() {
         if (mPhysicaloid == null)
             mPhysicaloid = new Physicaloid(BalanduinoActivity.activity);
 
